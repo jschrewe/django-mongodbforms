9f94ba5d5c9fca64022185cdc7d90c44454ff2c4
Jan Schrewe <jan@schafproductions.com>
2012-03-04 17:09:42 +0100

    Version bump and more info in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 from subprocess import call
 
 def convert_readme():
@@ -11,14 +11,25 @@ def convert_readme():
     return open('README.txt').read()
 
 setup(name='mongodbforms',
-    version='0.1.3',
+    version='0.1.4',
     description="An implementation of django forms using mongoengine.",
     author='Jan Schrewe',
     author_email='jan@schafproductions.com',
     url='http://www.schafproductions.com/projects/django-mongodb-forms/',
     packages=['mongodbforms',],
-    package_data={
-    },
+    package_data={},
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Framework :: Django',
+    ],
     license='New BSD License',
     long_description=convert_readme(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=['setuptools', 'django>=1.3', 'mongoengine',],
 )
