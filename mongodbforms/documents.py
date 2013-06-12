e6a6fbbe0bd3d10dd790828a88a0a5765343cd58
Matthew Gerring <gerring.matthew@gmail.com>
2013-06-11 21:55:44 -0700

    making parent_document into a keyword argument on EmbeddedDocumentForm to avoid a problem with formsets

--- mongodbforms/documents.py
+++ mongodbforms/documents.py
@@ -476,7 +476,7 @@ def documentform_factory(document, form=DocumentForm, fields=None, exclude=None,
 
 
 class EmbeddedDocumentForm(with_metaclass(DocumentFormMetaclass, BaseDocumentForm)):
-    def __init__(self, parent_document, *args, **kwargs):
+    def __init__(self, parent_document = parent_document, *args, **kwargs):
         super(EmbeddedDocumentForm, self).__init__(*args, **kwargs)
         self.parent_document = parent_document
         if self._meta.embedded_field is not None and not \
@@ -839,6 +839,7 @@ class EmbeddedDocumentFormSet(BaseInlineDocumentFormSet):
         defaults.update(kwargs)
         form = super(EmbeddedDocumentFormSet, self)._construct_form(i, **defaults)
         return form
+
     
     def save(self, commit=True):
         # Don't try to save the new documents. Embedded objects don't have
