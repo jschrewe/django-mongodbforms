d8cc4d4f9c6c08039bf4b0122ff15a5ccfd105c9
Naison Souza <naison.souza@gmail.com>
2014-10-04 15:45:39 -0300

    get_verbose_name fix for new django 1.7

--- mongodbforms/documentoptions.py
+++ mongodbforms/documentoptions.py
@@ -4,9 +4,12 @@ from types import MethodType
 
 from django.db.models.fields import FieldDoesNotExist
 from django.utils.text import capfirst
-from django.db.models.options import get_verbose_name
 from django.utils.functional import LazyObject
 from django.conf import settings
+try:
+    from django.db.models.options import get_verbose_name
+except ImportError:
+    from django.utils.text import camel_case_to_spaces as get_verbose_name
 
 from mongoengine.fields import ReferenceField, ListField
 
